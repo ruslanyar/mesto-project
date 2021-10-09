f5d3b182bea8cb532c285ab46c282b15d19fca8d
ruslanyar <styxrus@yandex.ru>
2021-10-09 19:35:11 +0300

    Continue working on script

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,37 @@
 const content = document.querySelector('.content');
-const popup = document.querySelector('.popup');
-const editBtn = document.querySelector('.profile__edit-button');
-const closeBtn = popup.querySelector('.popup__close-button');
-const submitBtn =popup.querySelector('.edit-profile__submit-button');
+const profileEditForm = document.querySelector('#profile-edit');
+const addCardsForm = document.querySelector('#add-cards');
+const popup = document.querySelectorAll('.popup');
+const editBtn = content.querySelector('.profile__edit-button');
+const addBtn = content.querySelector('.profile__add-button');
+const profileCloseBtn = document.querySelector('#profileCloseBtn');
+const addCardsCloseBtn = document.querySelector('#addCardsCloseBtn');
+const profileSubmitBtn = document.querySelector('#profileSubmitBtn');
+const addCardsSubmitBtn = document.querySelector('#addCardsSubmitBtn');
 
 let profileName = content.querySelector('.profile__name');
 let profileJob = content.querySelector('.profile__job');
-let inputName = popup.querySelector('.edit-profile__input_name');
-let inputJob = popup.querySelector('.edit-profile__input_job');
+let inputName = document.querySelector('#name');
+let inputJob = document.querySelector('#job');
+let cardName = document.querySelectorAll('.elements__caption-text');
 
 function editProfile() {
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
 
-  popup.classList.add('popup_opened');
+  profileEditForm.classList.add('popup_opened');
+}
+
+function addCards() {
+  addCardsForm.classList.add ('popup_opened');
 }
 
 function closeForm() {
-  popup.classList.remove('popup_opened');
+  profileEditForm.classList.remove('popup_opened');
+  addCardsForm.classList.remove('popup_opened');
 }
 
-function formSubmit(evt) {
+function editFormSubmit(evt) {
   evt.preventDefault();
 
   profileName.textContent = inputName.value;
@@ -29,6 +40,16 @@ function formSubmit(evt) {
   closeForm();
 }
 
+// function addFormSubmit(evt) {
+//   evt.preventDefault();
+
+
+
+//   closeForm();
+// }
+
 editBtn.addEventListener('click', editProfile);
-closeBtn.addEventListener('click', closeForm);
-submitBtn.addEventListener('click', formSubmit);
+profileCloseBtn.addEventListener('click', closeForm);
+addCardsCloseBtn.addEventListener('click', closeForm);
+addBtn.addEventListener('click', addCards);
+
