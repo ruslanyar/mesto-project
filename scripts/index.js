e9e46122ee4c77c936f660ab3aa982e7a4f71da8
ruslanyar <styxrus@yandex.ru>
2021-10-12 21:58:51 +0300

    Added addCard functional

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,16 @@ const editBtn = content.querySelector('.profile__edit-button');
 const addBtn = content.querySelector('.profile__add-button');
 const closeBtn = popups.querySelectorAll('.popup__close-button');
 const submit = popups.querySelectorAll('.form');
+const profileName = content.querySelector('.profile__name');
+const profileJob = content.querySelector('.profile__job');
+const inputName = popups.querySelector('.form__input_name');
+const inputJob = popups.querySelector('.form__input_job');
+const cardTemplate = document.querySelector('.cards-template').content;
+const cardElement = cardTemplate.querySelector('.elements__list-item').cloneNode(true);
 
-let profileName = content.querySelector('.profile__name');
-let profileJob = content.querySelector('.profile__job');
-let inputName = popups.querySelector('.form__input_name');
-let inputJob = popups.querySelector('.form__input_job');
-
+function defaultCards() {
+  
+}
 // функция открытия формы профиля
 function editFormHandler() {
   inputName.value = profileName.textContent;
@@ -38,8 +42,13 @@ function addFormHandler() {
 }
 // функция создания новой карточки
 function createCard() {
-  const cardTemplate = document.querySelector('.cards-template').content;
-  const cardElement = cardTemplate.querySelector('.elements__list-item').cloneNode(true);
+  const cardTitle = cardElement.querySelector('.elements__caption-text');
+  const cardLink = cardElement.querySelector('.elements__image');
+  const inputCardName = popups.querySelector('.form__input_place-name');
+  const inputCardLink = popups.querySelector('.form__input_link');
+
+  cardTitle.textContent = inputCardName.value;
+  cardLink.srcset = inputCardLink.value;
 
   content.querySelector('.elements__list').prepend(cardElement);
 }
@@ -60,3 +69,12 @@ closeBtn[1].addEventListener('click', closeForm);
 // слушатель отправки формы профиля
 submit[0].addEventListener('submit', submitFormHandler);
 submit[1].addEventListener('submit', submitAddCardForm);
+
+
+
+// const cardTemplate = document.querySelector('.cards-template').content;
+//   const cardElement = cardTemplate.querySelector('.elements__list-item').cloneNode(true);
+//   const cardTitle = cardElement.querySelector('.elements__caption-text');
+//   const cardLink = cardElement.querySelector('.elements__image');
+//   const inputCardName = popups.querySelector('.form__input_place-name');
+//   const inputCardLink = popups.querySelector('.form__input_link');
